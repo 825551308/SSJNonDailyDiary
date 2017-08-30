ba12794ab5df76518c2ea1f1d459ce7d016e67fd
jinshanshan <jinshanshan@jinshanshandeMac-mini.local>
2017-08-30 09:21:07 +0800

    Initial Commit

---
+++ SSJNonDailyDiary/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SSJNonDailyDiary
+//
+//  Created by 金汕汕 on 2017/8/30.
+//  Copyright © 2017年 ccs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
